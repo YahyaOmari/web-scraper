428dffe8a6afd61a2527f6ffba3726f8d634beab
YahyaOmari <yahya.omari@outlook.com>
2021-06-01 09:39:37 +0300

    changed some things

--- web_scraper.py
+++ web_scraper.py
@@ -14,23 +14,12 @@ def get_citations_needed_report(URL):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     citation = soup.find_all('sup', class_="noprint Inline-Template Template-Fact")
-    # print(citation)
-    citation_list = []
-    counter = 0
+    
+    result = ''
     
     for i  in citation:
-        if counter == 0:
-            citation_list.append(i.parent.text)
-            counter = 1
-        else:
-            if i.parent.text in citation_list:
-                continue
-            else:
-                citation_list.append(i.parent.text)
-                counter = counter + 1
-        # print(citation_list[counter-1])
-        
-    return(citation_list[counter-1])
+        result += f'{i.parent.text}\n'
+    return result
 
 URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
-get_citations_needed_report(URL)
+print(get_citations_needed_report(URL))
